1ade1a0ab8cb286453469632a742053cda87f60c
rosinni <ros.rm169@gmail.com>
2025-05-30 20:55:28 +0200

    update validateVMs

--- exercises/05-Verify-Configuration/validateVMs.js
+++ exercises/05-Verify-Configuration/validateVMs.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const path = require('path');
 
 function parseVMConfig(filePath) {
   const config = fs.readFileSync(filePath, 'utf8');
@@ -21,6 +20,8 @@ function validateVMs(configPath) {
   const machines = parseVMConfig(configPath);
 
   const results = {
+    foundWindows: false,
+    foundDebian: false,
     windowsHasNatNetwork: false,
     windowsNatNetworkConnected: false,
     debianHasNatNetwork: false,
@@ -32,7 +33,10 @@ function validateVMs(configPath) {
   let debianNatNetworkName = '';
 
   machines.forEach(machine => {
-    if (machine.name === 'windows') {
+    const osType = machine.ostype?.toLowerCase() || '';
+
+    if (osType.includes('windows')) {
+      results.foundWindows = true;
       if (machine.nic1 === 'natnetwork') {
         results.windowsHasNatNetwork = true;
         windowsNatNetworkName = machine.nat_network1;
@@ -40,7 +44,8 @@ function validateVMs(configPath) {
           results.windowsNatNetworkConnected = true;
         }
       }
-    } else if (machine.name === 'deb') {
+    } else if (osType.includes('debian') || osType.includes('linux')) {
+      results.foundDebian = true;
       if (machine.nic1 === 'natnetwork') {
         results.debianHasNatNetwork = true;
         debianNatNetworkName = machine.nat_network1;
@@ -54,6 +59,7 @@ function validateVMs(configPath) {
   if (
     results.windowsHasNatNetwork &&
     results.debianHasNatNetwork &&
+    windowsNatNetworkName &&
     windowsNatNetworkName === debianNatNetworkName
   ) {
     results.sameNatNetwork = true;
@@ -62,5 +68,73 @@ function validateVMs(configPath) {
   return results;
 }
 
-
 module.exports = { validateVMs };
+
+
+
+// const fs = require('fs');
+// const path = require('path');
+
+// function parseVMConfig(filePath) {
+//   const config = fs.readFileSync(filePath, 'utf8');
+//   const machines = config.split('---------').map(section => {
+//     const lines = section.split('\n').filter(line => line.trim() !== '');
+//     const machine = {};
+//     lines.forEach(line => {
+//       const [key, value] = line.split('=').map(str => str.trim().replace(/"/g, ''));
+//       if (key && value) {
+//         machine[key] = value;
+//       }
+//     });
+//     return machine;
+//   });
+//   return machines.filter(machine => Object.keys(machine).length > 0);
+// }
+
+// function validateVMs(configPath) {
+//   const machines = parseVMConfig(configPath);
+
+//   const results = {
+//     windowsHasNatNetwork: false,
+//     windowsNatNetworkConnected: false,
+//     debianHasNatNetwork: false,
+//     debianNatNetworkConnected: false,
+//     sameNatNetwork: false,
+//   };
+
+//   let windowsNatNetworkName = '';
+//   let debianNatNetworkName = '';
+
+//   machines.forEach(machine => {
+//     if (machine.name === 'windows') {
+//       if (machine.nic1 === 'natnetwork') {
+//         results.windowsHasNatNetwork = true;
+//         windowsNatNetworkName = machine.nat_network1;
+//         if (machine.cableconnected1 === 'on') {
+//           results.windowsNatNetworkConnected = true;
+//         }
+//       }
+//     } else if (machine.name === 'deb') {
+//       if (machine.nic1 === 'natnetwork') {
+//         results.debianHasNatNetwork = true;
+//         debianNatNetworkName = machine.nat_network1;
+//         if (machine.cableconnected1 === 'on') {
+//           results.debianNatNetworkConnected = true;
+//         }
+//       }
+//     }
+//   });
+
+//   if (
+//     results.windowsHasNatNetwork &&
+//     results.debianHasNatNetwork &&
+//     windowsNatNetworkName === debianNatNetworkName
+//   ) {
+//     results.sameNatNetwork = true;
+//   }
+
+//   return results;
+// }
+
+
+// module.exports = { validateVMs };
