e6ff13ce4b9db3cdfaefb98bf8c20609d6e89872
rosinni <ros.rm169@gmail.com>
2025-05-31 17:20:43 +0200

    update test

--- exercises/05-Verify-Configuration/validateVMs.js
+++ exercises/05-Verify-Configuration/validateVMs.js
@@ -6,9 +6,11 @@ function parseVMConfig(filePath) {
     const lines = section.split('\n').filter(line => line.trim() !== '');
     const machine = {};
     lines.forEach(line => {
-      const [key, value] = line.split('=').map(str => str.trim().replace(/"/g, ''));
-      if (key && value) {
-        machine[key] = value;
+      if (line.includes('=')) {
+        const [key, value] = line.split('=').map(str => str.trim().replace(/"/g, ''));
+        if (key && value) {
+          machine[key] = value;
+        }
       }
     });
     return machine;
@@ -35,20 +37,23 @@ function validateVMs(configPath) {
   machines.forEach(machine => {
     const osType = machine.ostype?.toLowerCase() || '';
 
+    // Prioriza nat_network1, pero si no existe, usa nat-network1
+    const natNetworkKey = machine['nat_network1'] || machine['nat-network1'];
+
     if (osType.includes('windows')) {
       results.foundWindows = true;
       if (machine.nic1 === 'natnetwork') {
         results.windowsHasNatNetwork = true;
-        windowsNatNetworkName = machine.nat_network1;
+        windowsNatNetworkName = natNetworkKey;
         if (machine.cableconnected1 === 'on') {
           results.windowsNatNetworkConnected = true;
         }
       }
-    } else if (osType.includes('debian') || osType.includes('linux')) {
+    } else if (osType.includes('debian') || osType.includes('linux') || osType.includes('ubuntu')) {
       results.foundDebian = true;
       if (machine.nic1 === 'natnetwork') {
         results.debianHasNatNetwork = true;
-        debianNatNetworkName = machine.nat_network1;
+        debianNatNetworkName = natNetworkKey;
         if (machine.cableconnected1 === 'on') {
           results.debianNatNetworkConnected = true;
         }
@@ -70,71 +75,3 @@ function validateVMs(configPath) {
 
 module.exports = { validateVMs };
 
-
-
-// const fs = require('fs');
-// const path = require('path');
-
-// function parseVMConfig(filePath) {
-//   const config = fs.readFileSync(filePath, 'utf8');
-//   const machines = config.split('---------').map(section => {
-//     const lines = section.split('\n').filter(line => line.trim() !== '');
-//     const machine = {};
-//     lines.forEach(line => {
-//       const [key, value] = line.split('=').map(str => str.trim().replace(/"/g, ''));
-//       if (key && value) {
-//         machine[key] = value;
-//       }
-//     });
-//     return machine;
-//   });
-//   return machines.filter(machine => Object.keys(machine).length > 0);
-// }
-
-// function validateVMs(configPath) {
-//   const machines = parseVMConfig(configPath);
-
-//   const results = {
-//     windowsHasNatNetwork: false,
-//     windowsNatNetworkConnected: false,
-//     debianHasNatNetwork: false,
-//     debianNatNetworkConnected: false,
-//     sameNatNetwork: false,
-//   };
-
-//   let windowsNatNetworkName = '';
-//   let debianNatNetworkName = '';
-
-//   machines.forEach(machine => {
-//     if (machine.name === 'windows') {
-//       if (machine.nic1 === 'natnetwork') {
-//         results.windowsHasNatNetwork = true;
-//         windowsNatNetworkName = machine.nat_network1;
-//         if (machine.cableconnected1 === 'on') {
-//           results.windowsNatNetworkConnected = true;
-//         }
-//       }
-//     } else if (machine.name === 'deb') {
-//       if (machine.nic1 === 'natnetwork') {
-//         results.debianHasNatNetwork = true;
-//         debianNatNetworkName = machine.nat_network1;
-//         if (machine.cableconnected1 === 'on') {
-//           results.debianNatNetworkConnected = true;
-//         }
-//       }
-//     }
-//   });
-
-//   if (
-//     results.windowsHasNatNetwork &&
-//     results.debianHasNatNetwork &&
-//     windowsNatNetworkName === debianNatNetworkName
-//   ) {
-//     results.sameNatNetwork = true;
-//   }
-
-//   return results;
-// }
-
-
-// module.exports = { validateVMs };
